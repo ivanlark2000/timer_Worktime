b54a16c6a6c655492be5484051a8d293ab57d956
ivan <ivaxen2000@gmail.com>
2022-04-22 16:59:00 +0200

    start ver. - 1.0

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import time
 import psutil
+import logging
 import threading
 from datetime import datetime
 from openpyxl.styles import Alignment, Font
@@ -13,12 +14,21 @@ def check_device():
     """Проверяет общую активность устройств в случае
     активности выводит True,
     если девайсы не используются выводит False """
+    def check_py():
+        """Проверяем активность процесса пйчарма"""
+        for proc in psutil.process_iter():
+            if proc.name() == 'pycharm.sh':
+                return True
+            else:
+                continue
+
     def check_mouse():
         """Проверяет активность мыши и возвращает
         True или False"""
+        logging.debug('check device', )
         mouse = Controller()
         check = mouse.position
-        time.sleep(2)
+        time.sleep(20)
         if mouse.position != check:
             return True
         else:
@@ -34,21 +44,12 @@ def check_device():
             else:
                 return True
 
-    if check_mouse():
+    if check_mouse() and check_py():
         return True
-    elif check_keyBoard():
+    elif check_keyBoard() and check_py():
         return True
     else:
         return False
-
-
-def check_py():
-    """Проверяем активность процесса пйчарма"""
-    for proc in psutil.process_iter():
-        if proc.name() == 'pycharm.sh':
-            return True
-        else:
-            continue
 
 
 def check_video():
@@ -66,52 +67,58 @@ def check_video():
         return False
 
 
-def get_time_proc():
+def get_time_proc(lock):
     """Записывает историю по работе юзера в приложении"""
     while True:
-        time.sleep(2)
-        while check_py():
-            time_end = time_start = datetime.now()
-            time.sleep(2)
-            while check_device():
-                time.sleep(30)
+        logging.debug('running')
+        time.sleep(5)
+        if check_device():
+            time_start = datetime.now()
+            while True:
+                time.sleep(3.25)
                 time_end = datetime.now()
-            if time_end > time_start and (time_end.second - time_start.second) > 5:
-                result = time_end - time_start
-                print(f'Работал в пайчарме {result.seconds}')
-                making_note(
-                    day=time_end.day,
-                    full_data=time_end.date(),
-                    time_of_start=time_start.time(),
-                    time_of_end=time_end.time(),
-                    proc='paycharm',
-                    second=result.seconds
+                if not check_device():
+                    result = time_end - time_start
+                    print(f'Работал в пайчарме {result.seconds} секунды')
+                    making_note(
+                        day=time_end.day,
+                        full_data=time_end.date(),
+                        time_of_start=time_start.time(),
+                        time_of_end=time_end.time(),
+                        proc='paycharm',
+                        second=result.seconds,
+                        lock=lock
                     )
+                    break
 
 
-def get_time_video():
+def get_time_video(lock):
     """Записывает историю при просмотре обучающего
     видео юзером"""
     while True:
-        time.sleep(1)
-        time_end = time_start = datetime.now()
-        while check_video():
-            time.sleep(1)
-            time_end = datetime.now()
-        if time_end > time_start and (time_end.second - time_start.second) > 5:
-            result = time_end - time_start
-            print(f'Смотрел обучающий видос {result.seconds} секунд')
-            making_note(
-                day=time_end.day,
-                full_data=time_end.date(),
-                time_of_start=time_start.time(),
-                time_of_end=time_end.time(),
-                proc='watch_video',
-                second=result.seconds
-            )
+        time.sleep(5)
+        logging.debug('running')
+        if check_video():
+            time_start = datetime.now()
+            while True:
+                time.sleep(2)
+                if not check_video():
+                    time_end = datetime.now()
+                    result = time_end - time_start
+                    print(f'Смотрел обучающий видос {result.seconds} секунд')
+                    making_note(
+                        day=time_end.day,
+                        full_data=time_end.date(),
+                        time_of_start=time_start.time(),
+                        time_of_end=time_end.time(),
+                        proc='watch_video',
+                        second=result.seconds,
+                        lock=lock
+                    )
+                    break
 
 
-def making_note(day, full_data, time_of_start, time_of_end, proc, second, thins=None, wb=None):
+def making_note(day, full_data, time_of_start, time_of_end, proc, second, lock):
     """Выполняет запись в таблицу XL
     :param: Day
     :param: full_Data
@@ -119,6 +126,7 @@ def making_note(day, full_data, time_of_start, time_of_end, proc, second, thins=
     :param: time_end
     :param: second
     """
+    lock.acquire()
     filename = 'timer.xlsx'
     font = Font(
         name='Calibri',
@@ -136,7 +144,7 @@ def making_note(day, full_data, time_of_start, time_of_end, proc, second, thins=
         shrink_to_fit=False,
         indent=0
     )
-
+    logging.debug('Send massage')
     wb = load_workbook(filename, read_only=False, keep_links=True, keep_vba=False)
     sheet = str(datetime.now().month)
     try:
@@ -156,14 +164,23 @@ def making_note(day, full_data, time_of_start, time_of_end, proc, second, thins=
         row.height = 25
         row.alignment = alignment
     finally:
+        lock.release()
         wb.save(filename)
         wb.close()
 
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s (%(threadName)-10s) %(message)s",
+)
+
+
 def main():
-    task = threading.Thread(target=get_time_video)
-    task.start()
-    get_time_proc()
+    lock = threading.Lock()
+    task_1 = threading.Thread(name='check_video', target=get_time_video, daemon=True, args=(lock,))
+    task_2 = threading.Thread(name='get_time_proc', target=get_time_proc, args=(lock,))
+    task_1.start()
+    task_2.start()
 
 
 if __name__ == '__main__':
